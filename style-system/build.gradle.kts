573cee1493cac97d5ef1aa240a284c5ee4b4ee59
vadwwxz <vadwwxz@gmail.com>
2023-08-02 22:46:38 +0300

    fix bug faced with fonts, remove several directories

--- style-system/build.gradle.kts
+++ style-system/build.gradle.kts
@@ -1,25 +1,10 @@
 plugins {
     id(libs.plugins.configurations.compose.multiplatform.get().pluginId)
-    alias(libs.plugins.moko.multiplatform.resources)
 }
 
 kotlin {
     jvm()
-    android()
-
-    sourceSets {
-        val commonMain by getting {
-            kotlin.srcDir("build/generated/moko/commonMain")
-        }
-
-        val androidMain by getting {
-            kotlin.srcDir("build/generated/moko/androidMain")
-        }
-
-        val jvmMain by getting {
-            kotlin.srcDir("build/generated/moko/jvmMain")
-        }
-    }
+    androidTarget()
 }
 
 dependencies {
@@ -29,16 +14,8 @@ dependencies {
     commonMainApi(compose.animation)
     commonMainApi(compose.foundation)
     commonMainApi(compose.materialIconsExtended)
-
-    commonMainImplementation(libs.moko.resources)
-    commonMainImplementation(libs.moko.resources.compose)
 }
 
 android {
     namespace = "com.cinematica.style.system"
-}
-
-multiplatformResources {
-    multiplatformResourcesPackage = "com.cinematica.style.system"
-    multiplatformResourcesClassName = "Resources"
 }
